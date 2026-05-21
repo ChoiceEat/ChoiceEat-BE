3f7d0d67d9fea014cc1c3c4109f0874c599d7680
조윤지 <yunji8533@naver.com>
2026-05-21 14:24:31 +0900

    feat: Swagger JWT 인증 설정 추가

--- src/main/java/com/choiceeat/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/choiceeat/backend/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.choiceeat.backend.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "JWT TOKEN";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Choice Eat API")
                         .description("Choice Eat API 명세서")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT Access Token 입력")));
     }
 }
-
