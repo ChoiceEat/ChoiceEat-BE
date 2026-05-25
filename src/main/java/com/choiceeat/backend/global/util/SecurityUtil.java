21ad28519a82baf2cddc37d6625821bba977ad38
hee0543 <chaehee0543@gmail.com>
2026-05-25 22:12:38 +0900

    fix:
    git add .
    
    

--- src/main/java/com/choiceeat/backend/global/util/SecurityUtil.java
+++ src/main/java/com/choiceeat/backend/global/util/SecurityUtil.java
@@ -1,5 +1,6 @@
 package com.choiceeat.backend.global.util;
 
+import com.choiceeat.backend.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -9,11 +10,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public final class SecurityUtil {
 
     public static Long getCurrentUserId() {
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
             throw new RuntimeException("인증 정보가 없습니다.");
         }
-        return Long.valueOf(authentication.getName());
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof User) {
+            return ((User) principal).getUserId(); // 여기서 직접 ID를 가져옵니다.
+        }
+        throw new RuntimeException("인증 객체의 형식이 올바르지 않습니다.");
     }
 }
