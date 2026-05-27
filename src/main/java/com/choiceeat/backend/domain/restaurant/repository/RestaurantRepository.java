b850a4bb3b7e98919620f7e3e11a58af7aec0e21
hee0543 <chaehee0543@gmail.com>
2026-05-28 01:37:56 +0900

    feat: RestaurantRespotitory에 카카오 장소 ID로 DB 검색하는 메서드 추가

--- src/main/java/com/choiceeat/backend/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/choiceeat/backend/domain/restaurant/repository/RestaurantRepository.java
@@ -3,6 +3,8 @@ package com.choiceeat.backend.domain.restaurant.repository;
 import com.choiceeat.backend.domain.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+import java.util.Optional;
 
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    Optional<Restaurant> findByKakaoPlaceId(String kakaoPlaceId);
 }
