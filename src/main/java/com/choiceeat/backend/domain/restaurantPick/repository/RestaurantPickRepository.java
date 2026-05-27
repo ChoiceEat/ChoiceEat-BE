f1fd8c526bea6106ad6a30cd01f140af41c92869
hee0543 <chaehee0543@gmail.com>
2026-05-28 00:18:13 +0900

    feat:RestaurantPickRepository 생성

--- src/main/java/com/choiceeat/backend/domain/restaurantPick/repository/RestaurantPickRepository.java
+++ src/main/java/com/choiceeat/backend/domain/restaurantPick/repository/RestaurantPickRepository.java
@@ -1,4 +1,8 @@
 package com.choiceeat.backend.domain.restaurantPick.repository;
 
-public interface RestaurantPickRepository {
+import com.choiceeat.backend.domain.restaurantPick.entity.RestaurantPick;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantPickRepository extends JpaRepository<RestaurantPick, Long> {
+
 }
